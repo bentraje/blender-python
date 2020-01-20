d838692da2e698a2e2c3e9670d6d55627ec005a5
Ben Traje <52109229+bentraje@users.noreply.github.com>
2020-01-20 15:44:49 +0000

    Update assign_obj_to_collection.py

--- general/assign_obj_to_collection.py
+++ general/assign_obj_to_collection.py
@@ -10,15 +10,11 @@ with open("D:\layer_list.json", "r") as read_file:
     
 for key, value in data.items():
     
-    print (key, value)
-    
     try:
         obj = bpy.data.objects[key]
     except:
         print (key +  " does not exist")
-        continue
-
-    bpy.context.scene.collection.objects.link(obj)    
+        continue   
 
     
     # Create a new collection if it doesn't exist
@@ -27,14 +23,24 @@ for key, value in data.items():
         bpy.context.scene.collection.children.link(col)
     else:
         col = bpy.data.collections[value]    
-        
+    
+    # Assign the object to the collection if it still not yet assigned
     for col in obj.users_collection:
         if col == bpy.data.collections[value]:
             pass
         else:
-            col.objects.link(obj)
+            col = bpy.data.collections[value]
+            try:
+                col.objects.link(obj)
+            except:
+                print (obj + ' already in collection ')
+                continue
     
     # Unlink Unnecessary Collections
     for col in obj.users_collection:
         if col != bpy.data.collections[value]:
             col.objects.unlink(obj)
+
+    # Remove parent on object to prevent that greyout object in the outline
+    bpy.data.objects[key].parent = None
+    
